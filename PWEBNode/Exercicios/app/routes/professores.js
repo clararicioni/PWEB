ea8fe34a775eb25051acf9eac0a272ab33b6a43a
CLARA RICIONI BERGAMO <bergamoclara9@gmail.com>
2025-06-09 12:13:11 -0300

    node: dbConnection

--- PWEBNode/Exercicios/app/routes/professores.js
+++ PWEBNode/Exercicios/app/routes/professores.js
@@ -1,27 +1,12 @@
+let dbConnection = require('../config/dbConnection.js');
 module.exports = function (app) {
     app.get('/informacao/professores', function (req, res) {
-        const sql = require('mssql');
-
-        const sqlConfig = {
-            user: 'BD2321021', //7 últimos dígitos do seu RA
-            password: 'Minecraft2020!',
-            database: 'BD',
-            server: 'APOLO',
-            options: {
-                encrypt: false,
-                trustServerCertificate: true,
-            }
-        }
         async function getProfessores() {
             try {
-                const pool = await sql.connect(sqlConfig);
-
+                const pool = await dbConnection();
                 const results = await pool.request().query('SELECT * from PROFESSORES')
-
                 //res.json(results.recordset);
-
-                 res.render('informacao/professores',{profs: results.recordset})
-
+                res.render('informacao/professores', { profs: results.recordset })
             } catch (err) {
                 console.log(err)
             }
