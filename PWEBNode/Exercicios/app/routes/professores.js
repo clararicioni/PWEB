31249a08f22ce28deca6282b9d2c3a8df8ffde40
CLARA RICIONI BERGAMO <bergamoclara9@gmail.com>
2025-06-09 11:37:22 -0300

    node: conexao bd

--- PWEBNode/Exercicios/app/routes/professores.js
+++ PWEBNode/Exercicios/app/routes/professores.js
@@ -1,5 +1,31 @@
 module.exports = function (app) {
     app.get('/informacao/professores', function (req, res) {
-        res.render("informacao/professores");
+        const sql = require('mssql');
+
+        const sqlConfig = {
+            user: 'BD2321021', //7 últimos dígitos do seu RA
+            password: 'Minecraft2020!',
+            database: 'BD',
+            server: 'APOLO',
+            options: {
+                encrypt: false,
+                trustServerCertificate: true,
+            }
+        }
+        async function getProfessores() {
+            try {
+                const pool = await sql.connect(sqlConfig);
+
+                const results = await pool.request().query('SELECT * from PROFESSORES')
+
+                //res.json(results.recordset);
+
+                 res.render('informacao/professores',{profs: results.recordset})
+
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        getProfessores();
     });
 }
